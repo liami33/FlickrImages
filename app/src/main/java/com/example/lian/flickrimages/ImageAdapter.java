9f0320b3b0930c481de586dc2553be4447ad1a32
liami33 <liami29@walla.com>
2015-09-29 01:33:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lian/flickrimages/ImageAdapter.java
@@ -0,0 +1,53 @@
+package com.example.lian.flickrimages;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Lian on 28/09/2015.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        //return mThumbIds.length;
+        return MainActivity.PHOTOS_PER_PAGE;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(250,250));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(4, 4, 4, 4);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+
+        imageView.setImageResource(R.drawable.loading_icon);
+        return imageView;
+    }
+
+
+}
